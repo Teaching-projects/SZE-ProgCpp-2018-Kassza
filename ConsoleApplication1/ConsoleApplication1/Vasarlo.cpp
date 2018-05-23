2b34644092451120a8c1c53f5ac95a168d09618a
Bef0r <39090682+Bef0r@users.noreply.github.com>
2018-05-23 19:28:35 +0200

    vasarlo osztályba létrehozva 2 randomgen. Az int típusú azt határozza meg kártya vagy kézpénzzel történt e a fizetés míg a stringes a bankártyaszámot generálja.

--- ConsoleApplication1/ConsoleApplication1/Vasarlo.cpp
+++ ConsoleApplication1/ConsoleApplication1/Vasarlo.cpp
@@ -1,11 +1,29 @@
 #include "stdafx.h"
 #include "Vasarlo.h"
+#include <cstdlib>
+#include <string>
 
 
 Vasarlo::Vasarlo()
 {
 }
 
+int Vasarlo::randomgen()
+{
+	return rand() % 1;
+
+}
+
+
+string Vasarlo::randomgenString()
+{
+	string osszefuzz;
+	for (int i = 0; i < 8; i++)
+	{
+		osszefuzz = osszefuzz + to_string(randomgen());
+	}
+	return string();
+}
 
 Vasarlo::~Vasarlo()
 {
--- ConsoleApplication1/ConsoleApplication1/Vasarlo.h
+++ ConsoleApplication1/ConsoleApplication1/Vasarlo.h
@@ -1,8 +1,16 @@
 #pragma once
+#ifndef Vasarlo_H
+#define Vasarlo_H
+#include <iostream>
+using namespace std;
 class Vasarlo
 {
+
 public:
+
 	Vasarlo();
+	int randomgen();
+	string randomgenString();
 	~Vasarlo();
 };
-
+#endif
